43ecd93bbc04d5f10bf7637fe207d91585c92b1c
Ash-2k3 <ash.me.cse@gmail.com>
2023-02-01 13:19:16 +0530

    Input decorator

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
+
+import {faCircle} from "@fortawesome/free-regular-svg-icons"
+import {faTimes , faPen} from "@fortawesome/free-solid-svg-icons"
 
 @Component({
+  template: '',
   selector: 'app-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.css']
 })
-export class IconComponent {
+export class IconComponent  {
+  
+ @Input() iconName:string = ''; // decorate the propery with @Input
 
+ faPen = faPen ;
+ faTimes = faTimes;
+ faCircle = faCircle
+ 
 }

9f13d5dc769280af134f4f16386b9036dd420c69
Ash-2k3 <ash.me.cse@gmail.com>
2023-02-01 10:59:11 +0530

    Configured Module files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconComponent } from './components/icon/icon.component';
 
+//font-awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//Toaster
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +21,9 @@ import { IconComponent } from './components/icon/icon.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
